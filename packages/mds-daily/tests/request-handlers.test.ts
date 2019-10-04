c7e5abd25f485f1b9eee50bcf364cbcba38c0b9c
macsj200 <maxjohansen@berkeley.edu>
2019-10-04 18:07:08 -0500

    initial foray into sinon-express-mock

--- packages/mds-daily/tests/request-handlers.test.ts
+++ packages/mds-daily/tests/request-handlers.test.ts
@@ -1,26 +1,18 @@
 import db from '@mds-core/mds-db'
 import Sinon from 'sinon'
+import { mockReq, mockRes } from 'sinon-express-mock'
 import { DailyApiRequest, DailyApiResponse } from '../types'
 import { getRawTripData } from '../request-handlers'
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
 
 describe('Request handlers', () => {
   describe('getRawTripData()', () => {
     it('handles a db read error in getRawTripData()', async () => {
       Sinon.replace(db, 'readEvents', Sinon.fake.rejects('fake-db-error'))
-      const res: DailyApiResponse = {} as DailyApiResponse
-      res.status = (code: number) => {
-        return {
-          send: () => code
-        } as any
-      }
-      await getRawTripData(
-        {
-          params: { trip_id: 'fake-trip-id' }
-        } as DailyApiRequest,
-        res as DailyApiResponse
-      )
+      const req: DailyApiRequest = mockReq({
+        params: { trip_id: 'fake-trip-id' }
+      })
+      const res: DailyApiResponse = mockRes()
+      await getRawTripData(req, res)
       Sinon.restore()
     })
   })
