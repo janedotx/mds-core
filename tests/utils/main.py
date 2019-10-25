8443b14c7df630c543df6ae8fa2bd8ec2f46ff3e
Max Johansen <macsj200@gmail.com>
2019-10-25 07:58:44 -0600

    skip packages

--- tests/utils/main.py
+++ tests/utils/main.py
@@ -5,14 +5,14 @@ import gen_stubs
 
 parser = argparse.ArgumentParser(description='MDS Integration Test Utility (ITU)')
 parser.add_argument('verb', type=str, nargs='+',
-                    help='action to perform (stub | codecov)')
-parser.add_argument('mds_core_path', nargs='+',
+                    help='stub:generate [package-names]')
+parser.add_argument('--mds_core_path', default=os.getcwd(),
                     help='path to mds-core')
 
 args = parser.parse_args()
 if args.verb[0].startswith('stub:'):
   action = args.verb[0].split(':')[1]
   if action == 'generate':
-    gen_stubs.write_test_files(args.mds_core_path[0])
+    gen_stubs.write_test_files(args.mds_core_path, args.verb[1:])
   elif action == 'clean':
-    gen_stubs.clean_test_files(args.mds_core_path[0])
+    gen_stubs.clean_test_files(args.mds_core_path)
