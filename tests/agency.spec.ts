f2f33fa08e02215ce15b9a38292b0b6577d1bcf9
Max Johansen <macsj200@gmail.com>
2019-10-25 17:09:57 -0600

    clean up

--- tests/agency.spec.ts
+++ tests/agency.spec.ts
@@ -1,93 +1,54 @@
 import requestPromise from 'request-promise'
 import assert from 'assert'
 import { getAuthToken } from './get-auth-token'
-import { gitHash, gitBranch, nodeVersion, packageVersion, isIsoDate } from './environment'
-import db from '../packages/mds-db'
-import cache from '../packages/mds-cache'
-import stream from '../packages/mds-stream'
-import { Device, VEHICLE_TYPES } from '../packages/mds-types'
-import { makeDevices, makeTelemetryInArea } from '../packages/mds-test-data'
-import { makeEventsWithTelemetry } from '../packages/mds-test-data'
-import { now } from '../packages/mds-utils'
-import { Telemetry } from '../packages/mds-types'
-import { Policy } from '../packages/mds-types'
-import { RULE_TYPES } from '../packages/mds-types'
-import { la_city_boundary } from '../packages/mds-policy/tests/la-city-boundary'
+import { gitHash, gitBranch, nodeVersion, packageVersion, isIsoDate, seedDb, resetDb } from './environment'
 
-const CITY_OF_LA = '1f943d59-ccc9-4d91-b6e2-0c5e771cbc49'
-const GEOGRAPHY_UUID = '8917cf2d-a963-4ea2-a98b-7725050b3ec5'
-const COUNT_POLICY_UUID = '72971a3d-876c-41ea-8e48-c9bb965bbbcc'
-
-const COUNT_POLICY_JSON: Policy = {
-  name: 'LADOT Mobility Caps',
-  description: 'Mobility caps as described in the One-Year Permit',
-  policy_id: COUNT_POLICY_UUID,
-  start_date: 1558389669540,
-  end_date: null,
-  prev_policies: null,
-  provider_ids: [],
-  rules: [
-    {
-      name: 'Greater LA',
-      rule_id: '47c8c7d4-14b5-43a3-b9a5-a32ecc2fb2c6',
-      rule_type: RULE_TYPES.count,
-      geographies: [GEOGRAPHY_UUID],
-      statuses: { available: [], unavailable: [], reserved: [], trip: [] },
-      vehicle_types: [VEHICLE_TYPES.bicycle, VEHICLE_TYPES.scooter],
-      maximum: 10,
-      minimum: 5
-    }
-  ]
-}
-
-describe('Agency', async function() {
-  // const devices: Device[] = makeDevices(7, now())
-  // const events = makeEventsWithTelemetry(devices, now() - 100000, CITY_OF_LA, 'trip_end')
-  // const telemetry: Telemetry[] = []
-  // devices.forEach(device => {
-  //   telemetry.push(makeTelemetryInArea(device, now(), CITY_OF_LA, 10))
-  // })
-  // const seedData = { devices, events, telemetry }
-  // await Promise.all([db.initialize(), cache.initialize(), stream.initialize()]).then(() => {
-  // })
-  // await Promise.all([db.seed(seedData), cache.seed(seedData)]).then(async () => {
-  //   const geography = { name: 'la', geography_id: GEOGRAPHY_UUID, geography_json: la_city_boundary }
-  //   await db.writeGeography(geography)
-  //   await db.writePolicy(COUNT_POLICY_JSON)
-  //   await db.publishPolicy(COUNT_POLICY_UUID)
-  // })
+describe('Agency', function() {
+  // TODO these should probably live in `beforeAll()`
+  // await resetDb()
+  // await seedDb()
   it('successfully initializes', async function() {
+    console.log('fire dis off')
     const res = await requestPromise({
       url: 'http://localhost/agency',
       auth: {
-        bearer: getAuthToken('', {
-          scope: "admin:all test:all"
-        }, '')
+        bearer: getAuthToken(
+          '',
+          {
+            scope: 'admin:all test:all'
+          },
+          ''
+        )
       },
       method: 'GET',
       json: true,
       resolveWithFullResponse: true
     })
+    console.log({ res })
     assert.strictEqual(res.statusCode, 200)
     assert.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
-    assert.strictEqual(res.headers['server'], 'istio-envoy')
-    assert.strictEqual(res.body.name, "@container-images/mds-agency")
+    assert.strictEqual(res.headers.server, 'istio-envoy')
+    assert.strictEqual(res.body.name, '@container-images/mds-agency')
     // fixme: get package version from env
     // assert.strictEqual(res.body.version, packageVersion())
     assert.strictEqual(isIsoDate(res.body.build.date), true)
     assert.strictEqual(res.body.build.branch, gitBranch())
     assert.strictEqual(res.body.build.commit, gitHash())
     assert.strictEqual(`v${res.body.node}`, nodeVersion())
-    assert.strictEqual(res.body.status, "Running")
+    assert.strictEqual(res.body.status, 'Running')
   })
 
   it('Inits a vehicle', async function() {
     const res = await requestPromise({
       url: 'http://localhost/agency/vehicles',
       auth: {
-        bearer: getAuthToken('', {
-          scope: "admin:all test:all"
-        }, '')
+        bearer: getAuthToken(
+          '',
+          {
+            scope: 'admin:all test:all'
+          },
+          ''
+        )
       },
       method: 'POST',
       json: true,
@@ -98,14 +59,14 @@ describe('Agency', async function() {
     })
     assert.strictEqual(res.statusCode, 200)
     assert.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
-    assert.strictEqual(res.headers['server'], 'istio-envoy')
-    assert.strictEqual(res.body.name, "@container-images/mds-agency")
+    assert.strictEqual(res.headers.server, 'istio-envoy')
+    assert.strictEqual(res.body.name, '@container-images/mds-agency')
     // fixme: get package version from env
     // assert.strictEqual(res.body.version, packageVersion())
     assert.strictEqual(isIsoDate(res.body.build.date), true)
     assert.strictEqual(res.body.build.branch, gitBranch())
     assert.strictEqual(res.body.build.commit, gitHash())
     assert.strictEqual(`v${res.body.node}`, nodeVersion())
-    assert.strictEqual(res.body.status, "Running")
+    assert.strictEqual(res.body.status, 'Running')
   })
 })
--- tests/environment.ts
+++ tests/environment.ts
@@ -1,4 +1,15 @@
 import { execSync } from 'child_process'
+import db from '../packages/mds-db'
+import cache from '../packages/mds-cache'
+import stream from '../packages/mds-stream'
+import { Device, VEHICLE_TYPES } from '../packages/mds-types'
+import { makeDevices, makeTelemetryInArea } from '../packages/mds-test-data'
+import { makeEventsWithTelemetry } from '../packages/mds-test-data'
+import { now } from '../packages/mds-utils'
+import { Telemetry } from '../packages/mds-types'
+import { Policy } from '../packages/mds-types'
+import { RULE_TYPES } from '../packages/mds-types'
+import { la_city_boundary } from '../packages/mds-policy/tests/la-city-boundary'
 
 export const gitHash = () => {
   return execSync('git rev-parse --short HEAD').toString().trim()
@@ -19,4 +30,52 @@ export const packageVersion = () => {
 
 export const isIsoDate = (s : string) => {
   return (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(s)) ? false : new Date(s).toISOString() == s;
+}
+
+export const seedDb = async () => {
+  const CITY_OF_LA = '1f943d59-ccc9-4d91-b6e2-0c5e771cbc49'
+  const GEOGRAPHY_UUID = '8917cf2d-a963-4ea2-a98b-7725050b3ec5'
+  const COUNT_POLICY_UUID = '72971a3d-876c-41ea-8e48-c9bb965bbbcc'
+
+  const COUNT_POLICY_JSON: Policy = {
+    name: 'LADOT Mobility Caps',
+    description: 'Mobility caps as described in the One-Year Permit',
+    policy_id: COUNT_POLICY_UUID,
+    start_date: 1558389669540,
+    end_date: null,
+    prev_policies: null,
+    provider_ids: [],
+    rules: [
+      {
+        name: 'Greater LA',
+        rule_id: '47c8c7d4-14b5-43a3-b9a5-a32ecc2fb2c6',
+        rule_type: RULE_TYPES.count,
+        geographies: [GEOGRAPHY_UUID],
+        statuses: { available: [], unavailable: [], reserved: [], trip: [] },
+        vehicle_types: [VEHICLE_TYPES.bicycle, VEHICLE_TYPES.scooter],
+        maximum: 10,
+        minimum: 5
+      }
+    ]
+  }
+
+  const devices: Device[] = makeDevices(7, now())
+  const events = makeEventsWithTelemetry(devices, now() - 100000, CITY_OF_LA, 'trip_end')
+  const telemetry: Telemetry[] = []
+  devices.forEach(device => {
+    telemetry.push(makeTelemetryInArea(device, now(), CITY_OF_LA, 10))
+  })
+  const seedData = { devices, events, telemetry }
+  await Promise.all([db.initialize(), cache.initialize(), stream.initialize()]).then(() => {
+  })
+  await Promise.all([db.seed(seedData), cache.seed(seedData)]).then(async () => {
+    const geography = { name: 'la', geography_id: GEOGRAPHY_UUID, geography_json: la_city_boundary }
+    await db.writeGeography(geography)
+    await db.writePolicy(COUNT_POLICY_JSON)
+    await db.publishPolicy(COUNT_POLICY_UUID)
+  })
+}
+
+export const resetDb = async () => {
+  await db.initialize()
 }
